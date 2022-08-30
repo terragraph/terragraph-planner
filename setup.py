64813a7be6c2003887b2bd08eab873f0389801d5
Wuwei Cai <78814740+wuweicaifb@users.noreply.github.com>
2022-08-30 11:51:28 -0700

    Release version 1.2.0 (#29)

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="terragraph_planner",
-    version="1.1.0",
+    version="1.2.0",
     description="""
 The Terragraph Planner is developed for operators to plan and optimize
  a mesh network using Terragraph's 60 GHz mmWave technology
